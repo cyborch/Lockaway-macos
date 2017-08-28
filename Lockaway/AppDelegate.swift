20f83e6abb982c420b17b63fcd66179cc9ae0284
Anders Borch <anders@cyborch.com>
2017-08-28 12:53:12 +0200

    Initial Commit

---
+++ Lockaway/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Lockaway
+//
+//  Created by Anders Borch on 8/28/17.
+//  Copyright © 2017 Anders Borch. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Lockaway/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Lockaway
+//
+//  Created by Anders Borch on 8/28/17.
+//  Copyright © 2017 Anders Borch. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
