7a8a01aacec2657ba2057e976bb342833d61279f
Anders Borch <anders@cyborch.com>
2017-09-01 12:03:39 +0200

    Added icons.

---
+++ Lockaway Helper/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Lockaway Helper
+//
+//  Created by Anders Borch on 9/1/17.
+//  Copyright © 2017 Anders Borch. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
